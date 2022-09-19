7030d8cbd16b583e358e6964ddabb40d6b5faeee
Yash Paudel <80587185+h4syy@users.noreply.github.com>
2022-09-19 10:29:12 +0545

    Basic fuctions
    Addition, Subtraction, Multiplication, Division functions were made.

--- ConsoleAppYear3/Program.cs
+++ ConsoleAppYear3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleAppYear3;
+
+Console.WriteLine("Hello");
+Class1 capp = new Class1();
+//capp.Addition();
+//capp.Subtraction();
+//capp.Multiplication();
+//capp.Division();
+capp.StringInterpolation();
